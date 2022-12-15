66a67b347b21a9ab642663b503dd919d660a6f38
Rubinhoalves <rubenslucas235@gmail.com>
2022-12-15 20:11:53 -0300

    atualizacao do trabralho js

---
+++ Tarefa1.js
@@ -0,0 +1,29 @@
+//1
+const myAge = 15;
+console.log(myAge)
+
+//2
+let earlyYears = 2;
+//os dois primeiros anos de um cachorro
+
+earlyYears *= 10.5; 
+console.log(earlyYears);
+
+//laterYear
+let laterYears =myAge -2;
+
+//multiplicao da idade do bixo
+laterYears *= 4 
+console.log(laterYears)
+
+
+
+
+let myAngeInDogYears = earlyYears + laterYears;
+//comentario do requisito 7
+const myName = 'Rubens'.toLowerCase()
+
+console.log(myName)
+
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em idade humana que é ${myAngeInDogYears} em anos caninos.`)
+
---
+++ Tarefa2.js
@@ -0,0 +1,18 @@
+//1
+const kelvin = 293;
+//linha de comentario da const kelvin
+
+//2
+const celsius =  kelvin - 273;
+//linha de comentario da const Celsius
+
+//3
+const fahrenheit = celsius *(9/5) + 32;
+//linha de comentario da const Fahrenheit
+
+//4
+ Math.floor(fahrenheit)
+
+ //linha de comentario da math floor
+
+ console.log(`A temperatura é ${kelvin} em escala Kelvin. A temperatura é ${celsius} em graus Celsius. A temperatura é ${fahrenheit} em Fahrenheit.`)
