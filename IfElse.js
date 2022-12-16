ffecd035a7dfe6e23976fa857de04225b6f1c14a
Rubinhoalves <rubenslucas235@gmail.com>
2022-12-16 17:28:04 -0300

    atualização

---
+++ IfElse.js
@@ -0,0 +1,22 @@
+//if
+let venda= false;
+if (venda) {
+    console.log('Hora de comprar') 
+} else {
+    console.log('Hora de esperar pela venda');
+}
+
+// Comparações
+let nivelDeFome = 9;
+if (nivelDeFome <= 9) {
+    console.log('Coma agora!') 
+} else {
+    console.log('Esperar chegar em casa.')   
+}
+//igualdade == ou ===
+//diferença != ou !==
+console.log('maçã'==='laranja')
+console.log(8 === 8)
+console.log(8 == 8)
+console.log(8 !== 8)
+console.log(8 != 8)
