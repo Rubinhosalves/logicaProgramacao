ee7a9f97921e22e92c5c0b6e55bd242f66fba6f1
Rubinhoalves <rubenslucas235@gmail.com>
2022-12-22 20:32:11 -0300

    atualizacao

--- Tarefa3.js
+++ Tarefa3.js
@@ -1,42 +1,85 @@
 //1
-let userName ='';
+const userName = 'Lucas Rubens';
+// Expressáo ternaria
+// A ? B : C
+userName ? console.log(`Olá ${userName}!`) : console.log('Olá!')
 
-//2
-userName ? console.log("Olá,lucas") : console.log('Olá.')
-
+// Pergunta que deve ser respondida
+const userQuestion = " Eu ficarei rico em 2023?"
 //3
-const userQuestion = "Eu vou conseguir vencer na vida?";
 
-//4
-console.log(userQuestion)
+const perguntaCompleta = userName ? `${userName} está perguntando: ${userQuestion}`
+    : `Usuário anônimo está perguntando: ${userQuestion}`
 
-//5
-let randomNumber = 6;
-console.log(Math.floor(Math.random() * 8));
+console.log(perguntaCompleta);
+console.log(userQuestion);
+
+// Gerando um valor aleatorio
+let randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
 
 //6
-let eightBall = "Os sinais apontam que sim.";
+let eightBall = "";
 
-//7
-if (randomNumber) {
+//7 
+// switch (randomNumber) {
+//     case 0: {
+//         eightBall = 'É uma certeza!';
+//         break;
+//     }
+//     case 1: {
+//         eightBall = 'É decididamente sim.';
+//         break;
+//     }
+//     case 2: {
+//         eightBall = 'Resposta nebulosa, tente de novo';
+//         break;
+//     }
+//     case 3: {
+//         eightBall = 'Não consigo prever agora.';
+//         break;
+//     }
+//     case 4: {
+//         eightBall = 'Não conte com isso.';
+//         break;
+//     }
+//     case 5: {
+//         eightBall = 'Minhas fontes dizem que não.';
+//         break;
+//     }
+//     case 6: {
+//         eightBall = 'Perspectiva não tão boa';
+//         break;
+//     }
+//     case 7: {
+//         eightBall = 'Os sinais apontam que sim';
+//         break;
+//     }
+//     default: {
+//         eightBall = 'Sei lá, poha'
+//     }
+// }
 
-}else if (eightBall === 'É uma certeza!') {
+if (randomNumber === 0) {
+    eightBall = 'É uma certeza!';
+} else if (randomNumber === 1) {
+    eightBall = 'É decididamente sim.';
+} else if (randomNumber === 2) {
+    eightBall = 'Resposta nebulosa, tente de novo';
+} else if (randomNumber === 3) {
+    eightBall = 'Não consigo prever agora.';
+} else if (randomNumber === 4) {
+    eightBall = 'Não conte com isso.';
+} else if (randomNumber === 5) {
+    eightBall = 'Minhas fontes dizem que não.!';
+} else if (randomNumber === 6) {
+    eightBall = 'Perspectiva não tão boa';
+} else if (randomNumber === 7) {
+    eightBall = 'Os sinais apontam que sim';
+} else (
+    eightBall = "Se lá, irmão"
+)
 
-}else if (eightBall === 'É decididamente assim!') {
 
-}else if (eightBall === 'Resposta nebulosa, tente de novo.') {
 
-}else if (eightBall === 'Não consigo prever agora.') {
-
-}else if (eightBall === 'Não conte com isso.') {
-
-}else if (eightBall === 'Minhas fontes dizem que não.') {
-
-}else if (eightBall === 'Perspectiva não tão boa.') {
-
-}else if (eightBall === 'Os Sinais Apontam Que Sim!') {
-
-}else 
-console.log ('Isso Não Vai Acontecer.');
-
-console.log (eightBall)
+console.log(eightBall)
