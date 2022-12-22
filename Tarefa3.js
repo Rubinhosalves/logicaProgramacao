826b2b540072f9d90abb9b245da4df13d7449d25
Rubinhoalves <rubenslucas235@gmail.com>
2022-12-22 17:57:27 -0300

    atualizacao

---
+++ Tarefa3.js
@@ -0,0 +1,42 @@
+//1
+let userName ='';
+
+//2
+userName ? console.log("Olá,lucas") : console.log('Olá.')
+
+//3
+const userQuestion = "Eu vou conseguir vencer na vida?";
+
+//4
+console.log(userQuestion)
+
+//5
+let randomNumber = 6;
+console.log(Math.floor(Math.random() * 8));
+
+//6
+let eightBall = "Os sinais apontam que sim.";
+
+//7
+if (randomNumber) {
+
+}else if (eightBall === 'É uma certeza!') {
+
+}else if (eightBall === 'É decididamente assim!') {
+
+}else if (eightBall === 'Resposta nebulosa, tente de novo.') {
+
+}else if (eightBall === 'Não consigo prever agora.') {
+
+}else if (eightBall === 'Não conte com isso.') {
+
+}else if (eightBall === 'Minhas fontes dizem que não.') {
+
+}else if (eightBall === 'Perspectiva não tão boa.') {
+
+}else if (eightBall === 'Os Sinais Apontam Que Sim!') {
+
+}else 
+console.log ('Isso Não Vai Acontecer.');
+
+console.log (eightBall)
