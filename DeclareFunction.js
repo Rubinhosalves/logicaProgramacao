53b06169c7a7e54417d443802ce7159830e82fac
Rubinhoalves <rubenslucas235@gmail.com>
2023-01-03 21:07:22 -0300

    atualizacao

---
+++ DeclareFunction.js
@@ -0,0 +1,23 @@
+/**
+ * 1 - Crie uma função que imprima um lembrete no console.
+ * Usando uma declaração de função, crie uma função chamada getReminder().
+ * 
+ * 2 - No corpo da função getReminder(), imprima o seguinte lembrete no
+ * console: 'Colocar água nas plantas.'
+ * 
+ * 3 - Crie uma função que imprima uma frase em espanhol útil para se usar em viagens.
+ * Usando uma declaração de função, crie uma função chamada greetInSpanish().
+ * 
+ * 4 - No corpo da função greetInSpanish(), imprima a frase no
+ * console: 'Buenas tardes.'
+ */
+function getReminder (){
+    console.log("Colocar aguas nas plantas.")
+}
+
+function greetInSpanish () {
+    console.log('Buenas tardes.')
+}
+
+getReminder();
+greetInSpanish();
